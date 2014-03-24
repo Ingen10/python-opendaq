f9cd184bc7cea9ae99083d5daef19b08d0a831db
Armando <armando@ingen10.com>
2014-03-24 13:43:16 +0100

    Updated versions of setup.py and README.md.

--- setup.py
+++ setup.py
@@ -1,26 +1,40 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from setuptools import setup, Command
+from opendaq import __version__
 
-DESCRIPTION = "GUI interface for openDAQ"
-LONG_DESCRIPTION = """`OpenDAQ <http://www.open-daq.com/>`_ is an open source
-acquisition instrument which provides several physical interaction capabilities
-such as analog inputs and outputs, digital inputs and outputs, timers and
-counters."""
+DESCRIPTION = 'Python binding for openDAQ hardware'
+LONG_DESCRIPTION = """
+`OpenDAQ <http://www.open-daq.com/>`_ is an open source acquisition instrument \
+which provides several physical interaction capabilities such as analog \
+inputs and outputs, digital inputs and outputs, timers and counters.
+"""
+
+class PyTest(Command):
+    user_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        import sys,subprocess
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
 
 
 setup(
-    name='easydaq',
-    version='0.1',
+    name='opendaq',
+    version=__version__,
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    author='Adrian Alvarez',
-    author_email='alvarez.lopez.adri@ingen10.com',
-    url='http://github.com/Ingen10/easydaq',
+    author='Juan Menendez',
+    author_email='juanmb@ingen10.com',
+    url='http://github.com/juanmb/python-opendaq',
     license='LGPL',
     platforms=['any'],
-    install_requires=['pyserial', 'wx'],
-    packages=['easydaq'],
+    install_requires=['pyserial'],
+    cmdclass = {'test': PyTest},
+    packages=['opendaq'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
@@ -32,5 +46,5 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Topic :: Software Development :: Scientific/Engineering'
         ' :: Libraries :: Python Modules',
-    ]
+    ],
 )
