30ea327d3670d0162798f1e33869df6d690b77fe
juanmb <juanmb@pulseingenieria.com>
2014-03-26 22:03:23 +0100

    Refactor test_common.py to unittest style

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,22 +1,23 @@
-import pytest
+import unittest
 from opendaq.common import crc, check_crc, CRCError, str2hex, mkcmd
 
 
-def test_crc():
-    assert crc('a') == '\x00\x61'
-    assert crc('\xff\xff') == '\x01\xfe'
-    assert crc('abcdefg') == '\x02\xbc'
-    assert crc('\xff'*300) == '\x2a\xd4'
+class TestCommon(unittest.TestCase):
+    def test_crc(self):
+        assert crc('a') == '\x00\x61'
+        assert crc('\xff\xff') == '\x01\xfe'
+        assert crc('abcdefg') == '\x02\xbc'
+        assert crc('\xff'*300) == '\x2a\xd4'
 
+    def test_check_crc(self):
+        assert check_crc('\x00\x61' + 'a') == 'a'
+        assert check_crc('\x02\xbc' + 'abcdefg') == 'abcdefg'
 
-def test_check_crc():
-    assert check_crc('\x00\x61' + 'a') == 'a'
-    assert check_crc('\x02\xbc' + 'abcdefg') == 'abcdefg'
+        self.assertRaises(CRCError, check_crc, '\x00a' + 'b')
 
-    with pytest.raises(CRCError):
-        check_crc('\x00a' + 'b')
+    def test_str2hex(self):
+        assert str2hex('\x12\x34\x56') == '12 34 56'
 
-
-def test_mkcmd():
-    assert str2hex(mkcmd(18, 'b', 1)) == '00 14 12 01 01'
-    assert str2hex(mkcmd(100, 'bH', 32, 1000)) == '01 73 64 04 20 03 e8'
+    def test_mkcmd(self):
+        assert str2hex(mkcmd(18, 'b', 1)) == '00 14 12 01 01'
+        assert str2hex(mkcmd(100, 'bH', 32, 1000)) == '01 73 64 04 20 03 e8'
